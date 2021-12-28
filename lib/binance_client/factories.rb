ad00db9bcd21871f2b478570e521cf63e437eaab
Ramon Tayag <ramon@bloom.solutions>
2021-12-28 15:07:28 +0800

    dev: add Network and Coin factories

--- lib/binance_client/factories.rb
+++ lib/binance_client/factories.rb
@@ -31,4 +31,38 @@ FactoryBot.define do
     sequence(:limit) { |n| n }
   end
 
+  factory :binance_client_coin, class: "BinanceClient::Coin" do
+    sequence(:coin) { |n| "ASSET#{n}" }
+    sequence(:name) { |n| "Asset #{n}" }
+    free { 0.001 }
+    freeze { 0.001 }
+    ipoable { 0.002 }
+    ipoing { 0.003 }
+    locked { 0.004 }
+    storage { 0.005 }
+    withdrawing { 0.006 }
+  end
+
+  factory :binance_client_network, class: "BinanceClient::Network" do
+    address_regex { "^(bnb1)[0-9a-z]{38}$" }
+    coin { "BTC" }
+    deposit_desc { "Wallet Maintenance" }
+    deposit_enable { false }
+    is_default { false }
+    memo_regex { "^[0-9A-Za-z\\-_]{1,120}$" }
+    min_confirm { 1 }
+    name { "BEP2" }
+    network { "BNB" }
+    reset_address_status { false }
+    special_tips { "Both a MEMO is required" }
+    un_lock_confirm { 0 }
+    withdraw_desc { "Wallet Maintenance" }
+    withdraw_enable { false }
+    withdraw_fee { "0.00000220" }
+    withdraw_integer_multiple { "0.00000001" }
+    withdraw_max { "9999999999.99999999" }
+    withdraw_min { "0.00000440" }
+    same_address { true }
+  end
+
 end
