5433607589ed96e5034f438f4784c637bbac4e3d
Ramon Tayag <ramon@bloom.solutions>
2022-03-29 22:01:36 +0800

    refactor: automate loading
    No need to specify all request, response, model files

---
+++ lib/binance_client/loader.rb
@@ -0,0 +1,42 @@
+module BinanceClient
+  class Loader
+
+    def self.call
+      self.new.call
+    end
+
+    def call
+      models
+      requests
+      responses
+    end
+
+    def models
+      require "binance_client/models/base_model"
+
+      Dir[File.join(binance_client_dir, "models/*.rb")].each do |f|
+        require f
+      end
+    end
+
+    def requests
+      require "binance_client/requests/base_request"
+      require "binance_client/requests/authenticated_base_request"
+      Dir[File.join(binance_client_dir, "requests/*.rb")].each do |f|
+        require f
+      end
+    end
+
+    def responses
+      require "binance_client/responses/base_response"
+      Dir[File.join(binance_client_dir, "responses/*.rb")].each do |f|
+        require f
+      end
+    end
+
+    def binance_client_dir
+      File.join(File.dirname(__FILE__))
+    end
+
+  end
+end
